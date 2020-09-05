66b1012a0b1e96f1064b2de458aafef0abf1a00f
Kappamalone <uzman.zawahir1@gmail.com>
2020-09-05 19:33:05 +0530

    update

--- public/client.js
+++ public/client.js
@@ -1,10 +1,25 @@
-function sendLogin(){
+function login(){
     let email = document.getElementById('loginEmail').value;
     let pass = document.getElementById('loginPass').value;
     console.log(email,pass)
     
     //send email and pass to server for verification
     fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email: email, pass: pass})
+    })
+}
+
+function signUp(){
+    let email = document.getElementById('loginEmail').value;
+    let pass = document.getElementById('loginPass').value;
+    console.log(email,pass)
+    
+    //send email and pass to server for verification
+    fetch('/signUp', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
--- server.js
+++ server.js
@@ -1,22 +1,59 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const user = require('./models/userModel')
 
+/*===========SETUP===========*/
 const app = express();
 const port = 3000;
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
+mongoDBSetup()
+
+/*
+exampleUser = new user({
+  username: 'test@123',
+  password: '123'
+})
+
+exampleUser
+  .save()
+  .then(item => console.log(item))
+  .catch(err => console.log(err))
+*/
 
 
-//use config package to retrieve env vars
+/*===========ROUTING===========*/
+app.get('/',(req,res) => {
+  res.redirect(307,'/register')
+})
+
+app.get('/register',(req,res) => {
+  res.sendFile(path.join(__dirname,'webpages','register.html'))
+})
+
+app.get('/login',(req,res) => {
+  res.sendFile(path.join(__dirname,'webpages','login.html'))
+})
+
+/*===========POST REQUESTS===========*/
+app.post('/signUp',(req,res) => {
+  //Check if email exists in database, and add user to it
+  console.log(req.body)
+})
+
+/*===========MONGO SETUP===========*/
 function mongoDBSetup() {
-    const pass = 'something'
-    let db = `mongodb+srv://Kappamalone:${pass}@kappamalone-cluster.u10jv.mongodb.net/diff?retryWrites=true&w=majority`;
+    let db = `mongodb+srv://Kappamalone:${process.env.MONGOPASS}@kappamalone-cluster.u10jv.mongodb.net/users?retryWrites=true&w=majority`;
   
     mongoose
       .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
       .then(() => console.log('Connected to MongoDB!'))
       .catch(err => console.log(err));
 }
+
+app.listen(port,() => {
+  console.log('Listening on port ' + port)
+})
